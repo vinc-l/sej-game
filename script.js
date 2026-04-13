fc616fb1bde6cde01842b4f2c469937fc84df4fb
vinc-l <vlimodya@gmail.com>
2026-04-13 18:47:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,13 @@ function getScore(type, difficulty) {
 }
 
 /* SHUFFLE ARRAY */
-const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 /* FILTER QUESTION BASED ON ID */
 function initQuiz() {
